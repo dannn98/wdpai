99ad1b2af555fa456894df5302d78081948270a8
Dawid <dannn.dawid@gmail.com>
2021-01-10 17:49:06 +0100

    Add photo view

---
+++ public/views/photo.php
@@ -0,0 +1,54 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="public/css/style.css">
+       <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <title>PHOTO PAGE</title>
+    </head>
+    <body>
+        <div class="home-container">
+            <header>
+                <div class="home-header-top">
+                    <div class="home-header">
+                        <img src="public/img/logo-white.svg">
+                        <div class="home-header-panel">
+                            <a href="/profil"><img src="public/img/user.svg"></a>
+                            <a href="/logout"><img src="public/img/logout.svg"></a>
+                        </div> 
+                    </div>
+                </div>
+                <div class="home-header-bottom">
+                    <nav class="home-nav">
+                        <a href="/home">HOME</a>
+                        <a href="/">PEOPLE</a>
+                        <a href="/photo">FOLDERS</a>
+                    </nav>
+                </div>
+            </header>
+            <div class="home-under-header"></div>
+            <div class="home-content">
+                <div class="photo-content-box">
+                    <div class="photo-content-box-img">
+                        <img src="public/uploads/4.png">
+                    </div>
+                    <div class="photo-content-box-properties">
+                        <div class="photo-properties-box">
+                            <h1>NieznanyNick</h1>
+                            <p>Typ użytkownika</p>
+                            <h2>69</h2>
+                            <div class="photo-comments">
+                                Comments
+                            </div>
+                            <div class="photo-add-comment">
+                                Add comments
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
