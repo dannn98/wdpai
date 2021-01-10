354606786a817bc00160aa668d348549d20c6f62
Dawid <dannn.dawid@gmail.com>
2021-01-10 19:26:46 +0100

    Add database connection

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,18 +2,25 @@
 
 require_once 'AppController.php';
 require_once __DIR__.'/../models/User.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 
 class SecurityController extends AppController
 {
     public function login(){
-        $user = new User("admin@admin.pl", 'admin', 'dawid', 'dabek');
+        $userRepository = new UserRepository();
 
-        if($this->isPost()) {
+        if(!$this->isPost()) {
             return $this->login('login');
         }
 
         $email = $_POST['email'];
         $password = $_POST['password'];
+
+        $user = $userRepository->getUser($email);
+
+        if(!$user) {
+            return $this->render('login', ['messages' => ['User not exist!']]);
+        }
 
         if ($user->getEmail() !== $email) {
             return $this->render('login', ['messages' => ['User with this email not exist!']]);
