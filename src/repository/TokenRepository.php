14c9bc9d7787fddb1428198307b94f387c27a4cf
Dawid <dannn.dawid@gmail.com>
2021-01-16 15:37:20 +0100

    Add TokenRepository.php

---
+++ src/repository/TokenRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once "Repository.php";
+
+class TokenRepository extends Repository
+{
+    public function addToken($key, $id_user) {
+        $hashed = md5($key.$_SERVER['REMOTE_ADDR']);
+
+        $stmt = $this->database->connect()->prepare('
+            INSERT INTO public.authentication_tokens (token, id_user) VALUES (?, ?)
+        ');
+
+        return $stmt->execute([
+            $hashed,
+            $id_user
+        ]);
+    }
+
+    public function checkToken($key): bool {
+        $hashed = md5($key.$_SERVER['REMOTE_ADDR']);
+
+        $stmt = $this->database->connect()->prepare('
+            SELECT * FROM public.authentication_tokens WHERE token = :token
+        ');
+        $stmt->bindParam(':token', $hashed, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($result == false) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function delToken($key) {
+        $hashed = md5($key.$_SERVER['REMOTE_ADDR']);
+
+        $stmt = $this->database->connect()->prepare('
+            DELETE FROM public.authentication_tokens WHERE token = :token
+        ');
+        $stmt->bindParam(':token', $hashed, PDO::PARAM_STR);
+        $stmt->execute();
+    }
+}
